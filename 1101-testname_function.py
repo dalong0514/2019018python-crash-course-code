d0dc925539fdedd4ade87f25a75a6ff07feda13d
dalong <dalong0514@126.com>
2020-08-29 00:38:04 +0800

    中间名单元测试

--- 1101-testname_function.py
+++ 1101-testname_function.py
@@ -1,13 +1,20 @@
 import unittest
 
-def get_formatted_name(first, last):
-    full_name = first + ' ' + last
+def get_formatted_name(first, last, middle=''):
+    if middle:
+        full_name = first + ' ' + middle + ' ' + last
+    else:
+        full_name = first + ' ' + last
     return full_name.title()
 
 class NamesTestCase(unittest.TestCase):
-    def test_upper(self):
+    def test_frist_last_name(self):
         formatted_name = get_formatted_name('feng', 'long')
         self.assertEqual(formatted_name, 'Feng Long')
+    
+    def test_frist_last_middle_name(self):
+        formattted_name = get_formatted_name('feng', 'long', 'da')
+        self.assertEqual(formattted_name, 'Feng Da Long')
 
 if __name__ == '__main__':
     unittest.main()
