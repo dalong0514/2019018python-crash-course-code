1fa21c16c9a97831a8faa7c7facdb7b123938770
dalong <dalong0514@126.com>
2020-08-29 01:03:10 +0800

    测试类重构加前置条件

--- 1101-testname_function.py
+++ 1101-testname_function.py
@@ -33,11 +33,20 @@ class AnonymousSurvey():
             print('- ' + response)
 
 class AnonymousSurveyTestCase(unittest.TestCase):
-    def test_store_single_response(self):
+    def setUp(self):
         question = 'What language did you first learn to speak?'
-        my_survey = AnonymousSurvey(question)
-        my_survey.store_response('English')
-        self.assertIn('English', my_survey.responses)
+        self.my_survey = AnonymousSurvey(question)
+        self.responses = ['English', 'Spanish', 'Mandarin']
+
+    def test_store_single_response(self):
+        self.my_survey.store_response(self.responses[0])
+        self.assertIn(self.responses[0], self.my_survey.responses)
+
+    def test_store_three_responses(self):
+        for response in self.responses:
+            self.my_survey.store_response(response)
+        for response in self.responses:
+            self.assertIn(response, self.my_survey.responses)
     
 if __name__ == '__main__':
     unittest.main()
